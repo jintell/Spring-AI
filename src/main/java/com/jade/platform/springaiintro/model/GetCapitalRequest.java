a754d99055a575275e46e8fbbb0ef33c0272c748
Josleke <josleke@gmail.com>
2024-05-07 19:05:06 +0100

    Output Response Formatting

---
+++ src/main/java/com/jade/platform/springaiintro/model/GetCapitalRequest.java
@@ -0,0 +1,7 @@
+package com.jade.platform.springaiintro.model;/**
+ *  @Author: Josiah Adetayo
+ *  @Email: josleke@gmail.com, josiah.adetayo@meld-tech.com
+ *  @Date: 5/7/24
+ */
+public record GetCapitalRequest() {
+}
